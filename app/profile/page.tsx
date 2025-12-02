cb885f2143ae6a9b04649ef7323ed0e0208fd139
Isaac Chan <isaacch@bu.edu>
2025-12-02 12:47:30 -0500

    updated styling

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -11,13 +11,16 @@ export default async function Page() {
 
     return (
         <Box sx={{ width: "100vw", height: "90vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <Paper elevation={3} sx={{ width: "65%", height: "60%", marginBottom: "5%", display: "flex", flexDirection: "column", gap: "2%", justifyContent: "center", alignItems: "center" }}>
+            <Paper elevation={3} sx={{ width: "65%", height: "60%", marginBottom: "5%", display: "flex", flexDirection: "column", gap: "1%", justifyContent: "center", alignItems: "center" }}>
+                <Typography variant="h4" component="h4" sx={{ marginBottom: "1%" }}>
+                        Profile
+                </Typography>
                 {/* use nullish coalescing operator to check if value on left is null or undefined and if so use the value on the right*/}
-                <img src={session.user?.image ?? ""} alt={`${session.user?.name} Profile Picture`} className="w-[100%] max-w-[20%]"/>
-                <Typography>
+                <img src={session.user?.image ?? ""} alt={`${session.user?.name} Profile Picture`} className="w-[20%]"/>
+                <Typography variant="h5" component="h4">
                     { session.user?.name }
                 </Typography>
-                <Typography>
+                <Typography variant="h5" component="h4">
                     { session.user?.email }
                 </Typography>
             </Paper>
