7928cdfe752c5401c2b1d782d6603c8dd1b16103
Isaac Chan <isaacch@bu.edu>
2025-12-02 11:34:57 -0500

    added auth + moved home component to server side since does not need client

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-import Home from "@/components/Home";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { signIn } from "@/auth";
 
-export default function HomePage() {
-  return (
-    <Home />
-  );
+export default function Home() {
+    return (
+        <Box sx={{ width: "100vw", height: "90vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <Paper elevation={3} sx={{ width: "65%", height: "60%", marginBottom: "5%", display: "flex", flexDirection: "column", gap: "20%"}}>
+                <Typography variant="h3" component="h1" sx={{ width: "100%", textAlign: "center", marginTop: "5%" }}>
+                    GitHub OAuth
+                </Typography>
+                <form
+                    action={async () => {
+                        "use server";
+                        await signIn("github", {redirectTo: "/profile" });
+                    }}
+                >
+                    <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+                        <Button type="submit" variant="contained" sx={{ width: "60%" }}>
+                            Sign in with GitHub
+                        </Button>
+                    </Box>
+                </form>
+            </Paper>
+        </Box>
+    );
 }
